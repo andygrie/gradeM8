3eafbe6d6500921d2db2c7b0151c25c0f437ad5b
Max Haider <max.haider@outlook.com>
2016-10-31 15:28:40 +0100

    testing

--- gradeM8Web/app.js
+++ gradeM8Web/app.js
@@ -29,7 +29,7 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/about', routes.about);
+app.get('/boert', routes.bört);
 app.get('/contact', routes.contact);
 
 http.createServer(app).listen(app.get('port'), function () {
--- gradeM8Web/routes/index.js
+++ gradeM8Web/routes/index.js
@@ -7,8 +7,8 @@ exports.index = function (req, res) {
     res.render('index', { title: 'Express', year: new Date().getFullYear() });
 };
 
-exports.about = function (req, res) {
-    res.render('about', { title: 'About', year: new Date().getFullYear(), message: 'Your application description page' });
+exports.bört = function (req, res) {
+    res.render('boert', { title: 'Title Bört', year: new Date().getFullYear(), message: 'Albört' });
 };
 
 exports.contact = function (req, res) {
