13ae057a8b3ae8b86578e5a94c15ee970b729e4b
Martin Laubreiter <Martin Laubreiter>
2016-10-31 18:53:42 +0100

    added some things to Gui and answered to requests

---
+++ app.js
@@ -0,0 +1,57 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [
+  'ngRoute',
+  'myApp.view1',
+  'myApp.view2',
+  'myApp.version'
+]).
+config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+  $locationProvider.hashPrefix('!');
+
+  $routeProvider.otherwise({redirectTo: '/view1'});
+}]);
+
+// Get the modal
+var modal = document.getElementById('myModal');
+var modal2 = document.getElementById('myModal2');
+
+// Get the button that opens the modal
+var btn = document.getElementById("addClassBtn");
+var btnSubject = document.getElementById("addSubjectBtn");
+var btn2 =document.getElementById("btnCancel")
+var btn3 =document.getElementById("btnCancel2")
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+var span2 = document.getElementsByClassName("close")[1];
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+btnSubject.onclick = function() {
+  modal2.style.display = "block";
+}
+btn2.onclick = function() {
+  modal.style.display = "none";
+}
+btn3.onclick = function() {
+  modal2.style.display = "none";
+}
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+span2.onclick = function() {
+  modal2.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+  if(event.target == modal2){
+    modal2.style.display = "none";
+  }
+}
