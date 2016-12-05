31d7b00466fb92b6330480e5c23c333414e0020c
Martin Laubreiter <Martin Laubreiter>
2016-12-05 23:02:29 +0100

    angular

--- js/controller/ctrlLogin.js
+++ js/controller/ctrlLogin.js
@@ -2,6 +2,7 @@ angular.module("moduleLogin", [])
 .controller("ctrlLogin", ["$scope", "constants", "$location",
                 function ($scope, constants, $location) {
 
+
     /*
     $scope.colTeachers = [
         {idUser: 1, forename: "Richard", surname: "Ludy"},
@@ -11,6 +12,12 @@ angular.module("moduleLogin", [])
     
     $scope.logIn = function (){
         //maybe check for selectedItem
+        console.log("working: ", $scope.user.name);
+        console.log("working: ", $scope.user.password);
+
+        constants.teacherName = $scope.user.name;
+        constants.teacherPassword = $scope.user.password;
+
         console.log("working: ", $scope.form.idUser);
         constants.teacherId = $scope.form.idUser;
         $location.path("/overview");
