65a0e51df654725475acf19a3f3d249fc3704151
Andy Grie <andy.grieszer@gmx.at>
2016-12-08 14:03:11 +0100

    returned classes now sorted

--- gradeM8Web/services/class.js
+++ gradeM8Web/services/class.js
@@ -162,6 +162,7 @@ exports.getClassesFromAD = function (req, res) {
                         });
                     }
                 }
+                classes.sort(function (a, b) { return (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0); }); 
                 res.send(classes);
             }
         });
