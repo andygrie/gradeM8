25ecc402e54145722dd7148b04bab8be9690bffa
Martin Laubreiter <Martin Laubreiter>
2016-12-09 13:27:50 +0100

    angular

--- js/controller/ctrlGroup.js
+++ js/controller/ctrlGroup.js
@@ -15,7 +15,7 @@ angular.module("moduleGroup", [])
     $scope.colSelectedPupils = [];
     $scope.colParticipations = [];
     $scope.colEvents = [];
-                    $scope.breadcrumb = "Group - " + sData_allData.data.idGradeGroup;
+                    $scope.breadcrumb = "Group - " + $routeParams.idGradeGroup;
                     $scope.show = true;
 
     $scope.getSubjectOfGroup = function(){
