d07c86a7647b1807d9fbb7afbaf9993722f10f68
Martin Laubreiter <Martin Laubreiter>
2016-12-10 16:19:18 +0100

    modal update

--- js/controller/ctrlOverview.js
+++ js/controller/ctrlOverview.js
@@ -13,6 +13,42 @@ angular.module("moduleOverview", [])
                         $scope.show = true;
                         $scope.data.currentSettingstab = "Period";
 
+
+                        $scope.items = [1,2,3,4,5];
+                        $scope.selected = [1];
+
+                        $scope.toggle = function (item, list) {
+                            var idx = list.indexOf(item);
+                            if (idx > -1) {
+                                list.splice(idx, 1);
+                            }
+                            else {
+                                list.push(item);
+                            }
+                        };
+
+                        $scope.exists = function (item, list) {
+                                return list.indexOf(item) > -1;
+                        };
+
+                        $scope.isIndeterminate = function() {
+                            return ($scope.selected.length !== 0 &&
+                            $scope.selected.length !== $scope.items.length);
+                        };
+
+                        $scope.isChecked = function() {
+                            return $scope.selected.length === $scope.items.length;
+                        };
+
+                        $scope.toggleAll = function() {
+                            if ($scope.selected.length === $scope.items.length) {
+                                $scope.selected = [];
+                            } else if ($scope.selected.length === 0 || $scope.selected.length > 0) {
+                                $scope.selected = $scope.items.slice(0);
+                            }
+                        };
+
+
     sData_groupsBySubjects.fillData().then(function(response){
         console.log(response);
         $scope.colGroupsBySubjects = sData_groupsBySubjects.data;
