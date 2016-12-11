fc596289741eb142d965fef258031b476baf7304
Martin Laubreiter <Martin Laubreiter>
2016-12-11 18:29:56 +0100

    probably killing everything

--- js/controller/ctrlEventGrading.js
+++ js/controller/ctrlEventGrading.js
@@ -1,4 +1,4 @@
-angular.module("moduleGroup", ['ngMaterial'])
+angular.module("moduleEvent", ['ngMaterial'])
     .controller("ctrlGroup", ["$scope", "$routeParams", "$location", "sData_pupilsByGroups", "sData_eventsByGroups",
         "sData_CUDHandler", "sData_allData", "sData_teaches", "sData_classes", "sData_pupilsByClass", "sData_participationsByEvent","$mdDialog",
         function ($scope, $routeParams, $location, sData_pupilsByGroups, sData_eventsByGroups,
