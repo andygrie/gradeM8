bef8f876cc501cce0cd1c558fd119eb6b077fa39
Martin Laubreiter <Martin Laubreiter>
2016-12-12 19:21:16 +0100

    e-mail date gui

--- js/controller/ctrlOverview.js
+++ js/controller/ctrlOverview.js
@@ -33,10 +33,8 @@ angular.module("moduleOverview", [])
                        var mydateFunction =  function(){
                             console.log("ready");
                             var date_input=$('input[name="date"]'); //our date input has the name "date"
-                            var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
                             date_input.datepicker({
                                 format: 'mm/dd/yyyy',
-                                container: container,
                                 todayHighlight: true,
                                 autoclose: true
                             });
