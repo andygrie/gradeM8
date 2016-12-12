53cac9193ce011f304517ef1cc00ae98301f9b89
Martin Laubreiter <Martin Laubreiter>
2016-12-12 19:07:43 +0100

    e-mail date gui

--- js/controller/ctrlOverview.js
+++ js/controller/ctrlOverview.js
@@ -17,6 +17,16 @@ angular.module("moduleOverview", [])
     $scope.show = true;
     $scope.data.currentSettingstab = "Period";
 
+                        $(document).ready(function(){
+                            var date_input=$('input[name="date"]'); //our date input has the name "date"
+                            var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
+                            date_input.datepicker({
+                                format: 'mm/dd/yyyy',
+                                container: container,
+                                todayHighlight: true,
+                                autoclose: true,
+                            });
+                        });
 
     $scope.status = '  ';
 
