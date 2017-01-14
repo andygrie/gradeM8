f15e8270c4ff4472ec18b451b0661d1441a5bfee
Martin Laubreiter <Martin Laubreiter>
2017-01-14 21:15:46 +0100

    updating gui

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 var app = angular.module("gradeM8", ["ngRoute",
-                                     "ngMaterial",
-                                     "moduleOverview",
-                                     "modulePupil",
-                                     "moduleGroup",
-                                     "moduleData",
-                                     "moduleWeb",
-                                     "moduleLogin",
-                                    "moduleEvent"
+                                        "ngMaterial",
+                                        "moduleOverview",
+                                        "modulePupil",
+                                        "moduleGroup",
+                                        "moduleData",
+                                        "moduleWeb",
+                                        "moduleLogin",
+                                        "moduleEvent"
                                     ]);
 app.constant('constants', {
     apiUrl: "http://gradem8.azurewebsites.net",
@@ -21,11 +21,11 @@ app.config(function($routeProvider, $mdDateLocaleProvider) {
   
   $routeProvider
   .when("/", {
-    templateUrl : "templates/styled_Login.html",
+    templateUrl : "templates/old_styled_Login.html",
     controller: "ctrlLogin"
   })
   .when("/overview", {
-    templateUrl : "templates/styled_Overview.html",
+    templateUrl : "templates/old_styled_Overview.html",
     controller: "ctrlOverview"
   })
   .when("/group/:idGradeGroup", {
