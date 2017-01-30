abc2106cf5b41ca95c240b49114487a8f8942f7e
Martin Laubreiter <Martin Laubreiter>
2017-01-31 00:58:10 +0100

    repairing login-screen scrolling

--- js/controller/ctrlLogin.js
+++ js/controller/ctrlLogin.js
@@ -13,7 +13,7 @@ angular.module("moduleLogin", [])
 
             $scope.breadcrumb = "login";
 
-            document.getElementsByTagName("body")[0].style.overflowY="hidden";
+            document.getElementsByTagName("body")[0].style.overflowY="hidden !important";
             /*
              $scope.colTeachers = [
              {idUser: 1, forename: "Richard", surname: "Ludy"},
